f1ead942f15b2755fe541c84315156de9a4d4443
David Glick <david@glicksoftware.com>
2024-11-02 12:41:48 -0700

    @@plone-addsite is no more

--- test/tests/plone-basics/run.sh
+++ test/tests/plone-basics/run.sh
@@ -35,4 +35,4 @@ get_auth() {
 [[ "$(get 'http://plone:8080')" == *"Welcome to Plone!"* ]]
 
 # Create a Plone site
-[[ "$(get_auth 'http://plone:8080/@@plone-addsite' "$(echo -n 'admin:admin' | base64)")" == *"Create a new site"* ]]
+[[ "$(get_auth 'http://plone:8080' "$(echo -n 'admin:admin' | base64)")" == *"Create a new site"* ]]
